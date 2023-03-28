f73a108dbe448636b222c76391ac628798a007c0
Abdu <abdulrahman.omar@student.moringaschool.com>
2023-03-28 11:06:46 +0300

    add owner_id FK

--- db/migrate/20230328075057_create_rider_bikes.rb
+++ db/migrate/20230328075057_create_rider_bikes.rb
@@ -3,6 +3,7 @@ class CreateRiderBikes < ActiveRecord::Migration[7.0]
     create_table :rider_bikes do |t|
       t.belongs_to :rider, null: false, foreign_key: true
       t.belongs_to :bike, null: false, foreign_key: true
+      t.belongs_to :owner, null: false, foreign_key: true
 
       t.timestamps
     end
